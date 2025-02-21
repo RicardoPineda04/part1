e17d792016d50e525ffd331b996d410dbe0f82e8
ricardodev04 <rapineda@innovacion.gob.sv>
2025-02-21 15:17:15 -0600

    1.7: unicafe, paso 2

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -6,19 +6,22 @@ const Title = ({ title }) => {
   )
 }
 
-const Content = ({ good, neutral, bad}) => {
+const Content = ({ good, neutral, bad, all, average, positive}) => {
   return (
     <div>
       <p>Good: {good}</p>
       <p>Neutral: {neutral}</p>
       <p>Bad: {bad}</p>
+      <p>All Clicks: {all}</p>
+      <p>Average: {average}</p>
+      <p>Positive: {positive} %</p>
     </div>
   )
 }
 
 const Button = ({ handleClick, text }) => {
   return (
-    <button onClick={ handleClick}>
+    <button onClick={ handleClick }>
       { text }
     </button>
   )
@@ -26,20 +29,47 @@ const Button = ({ handleClick, text }) => {
 
 const App = () => {
   // guarda los clics de cada botón en su propio estado
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [ good, setGood ] = useState(0)
+  const [ neutral, setNeutral ] = useState(0)
+  const [ bad, setBad ] = useState(0)
+  const [ click, setClicks ] = useState(0)
+  const [ average, setAverage ] = useState(0)
+  const [ positive, setPositive ] = useState(0)
 
   const handleClickGood = () => {
+    const positive = good + 1;
     setGood( good + 1 );
+    const newClick = click + 1;
+    setClicks(newClick);
+    handleAverage(newClick);
+    handlePositive(positive , newClick);
   }
 
   const handleClickNeutral = () => {
     setNeutral( neutral + 1 );
+    const newClick = click + 1;
+    setClicks(newClick);
+    handleAverage(newClick);
+    handlePositive(good , newClick);
   }
 
   const handleClickBad = () => {
     setBad( bad + 1 );
+    const newClick = click + 1;
+    setClicks(newClick);
+    handleAverage(newClick);
+    handlePositive(good , newClick);
+  }
+  
+  const handleAverage = (clicks) => {  
+    const total = (good - bad); 
+    const avg = (total) / clicks;
+    setAverage(avg);
+  }
+
+  const handlePositive = (positive, clicks) => {   
+    const total = (positive / clicks) * 100;
+    setPositive(total)
   }
 
   return (
@@ -49,7 +79,7 @@ const App = () => {
       <Button text = { 'Neutral' } handleClick={() => handleClickNeutral()}/>
       <Button text = { 'Bad' } handleClick={() => handleClickBad()}/>
       <Title title = { 'Statics' } />
-      <Content good = { good } neutral = { neutral } bad = { bad } />
+      <Content good = { good } neutral = { neutral } bad = { bad } all = { click } average = { average } positive = { positive } />
     </div>
   )
 }
